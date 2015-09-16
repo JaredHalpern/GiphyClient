19086f7ff7face9216c3261465619df01aaf214f
Jared Halpern <jared@bytemason.com>
2015-09-15 22:49:58 -0400

    Don't add semi-colons to pre-processing directives. Add a small font.

--- GiphyClient/Constants.h
+++ GiphyClient/Constants.h
@@ -21,10 +21,10 @@
 
 #pragma mark - Colors
 
-#define kColorYellow            [UIColor colorWithRed:247./256. green:200./256. blue:44./256. alpha:1.0];
-#define kColorLightBlue         [UIColor colorWithRed:145./256. green:223./256. blue:221./256. alpha:1.0];
-#define kColorDarkBlue          [UIColor colorWithRed:79./256. green:136./256. blue:134./256. alpha:1.0];
-
+#define kColorYellow            [UIColor colorWithRed:247./256. green:200./256. blue:44./256. alpha:1.0]
+#define kColorLightBlue         [UIColor colorWithRed:145./256. green:223./256. blue:221./256. alpha:1.0]
+#define kColorDarkBlue          [UIColor colorWithRed:79./256. green:136./256. blue:134./256. alpha:1.0]
+#define kColorDarkestBlue       [UIColor colorWithRed:44./256. green:71./256. blue:70./256. alpha:1.0]
 #pragma mark - Timing
 
 #define kTimeSearchVCSlide      0.25
@@ -36,7 +36,8 @@
 
 #pragma mark - Fonts
 
-#define kFontRegular          [UIFont fontWithName:@"Avenir Book" size:18.0]
+#define kFontRegular            [UIFont fontWithName:@"Avenir Book" size:18.0]
+#define kFontSmall              [UIFont fontWithName:@"Avenir Book" size:12.0]
 
 #pragma mark - API Related
 
